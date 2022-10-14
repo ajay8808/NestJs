bcda091d37a63a4c059fffab26be992afbfae310
ajay8808 <48876700+ajay8808@users.noreply.github.com>
2022-10-14 23:01:44 +0900

    css 수정

--- src/component/Top.js
+++ src/component/Top.js
@@ -3,10 +3,12 @@ import Gnb from "./Gnb";
 
 export default function Top() {
     return (
-      <div> 
-        <img src="/images/image.png" />
+      <div style={{diplay:"flex",paddingTop:20}}>
+        <div style={{flex:"100px 0 0"}}>
+        <img src="/images/image.png" style={{display:"block", width:80}} />
         <Header as="h1">코딩</Header>
         <Gnb/>
+        </div>
         </div>
     )
   }
