4d0411895172af0386a8b1dbb33dc09a12f10894
ajay8808 <48876700+ajay8808@users.noreply.github.com>
2022-10-21 22:48:27 +0900

    itemlist 추가

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react';
+import ItemList from '../src/component/ItemList';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [list,setList] = useState([]);
+
+  const API_URL = 
+  "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+    const getData = () =>{
+      Axios.get(API_URL).then(res =>{
+        console.log(res);
+        setList(res.data);
+      });
+    }
+    useEffect(()=>{
+      getData();
+    },[]);
   return (
+    <div>
+    <Head> Home </Head>
     <div> create-next-app</div>
+    <ItemList list={list}/>
+    </div>
   )
 }
