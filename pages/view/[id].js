77c1b80838bd290af59c066fc044a3cdc9883428
ajay8808 <48876700+ajay8808@users.noreply.github.com>
2022-11-13 22:08:23 +0900

    아이템 상세페이지

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,5 +1,26 @@
+import { useRouter } from "next/router"
+import { useEffect } from "react";
+
+
+
 export default function About() {
-    return (
-      <div> About</div>
+  const router = useRouter();
+  const {id} = router.query;
+
+  const API_URL = `http://makeup-api.herokuapp.com/api/v1/product/${id}`
+  
+  function getData() {
+    Axios.get(API_URL).then((res)=>{
+      console.log(res.data);
+    });
+  }
+  useEffect(()=>{
+    if(id && id>0){
+      getData();
+    }
+  },[id])
+  
+  return (
+      <div> Post :{id}</div>
     )
   }
