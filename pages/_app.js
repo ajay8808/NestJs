36e702c2338c90d44b78e499d2a37ca62d32cb9b
ajay8808 <48876700+ajay8808@users.noreply.github.com>
2022-11-20 22:27:44 +0900

    menu 수정

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 import Footer from '../src/component/Footer'
-import Top from '../src/component/Top'
+import Top from '../src/component/Top' 
 
 function MyApp({ Component, pageProps }) {
   return (
